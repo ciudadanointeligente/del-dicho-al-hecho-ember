5f3b5ef1b8b1f50605c42a4d5c13f3a1ed443ce3
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-02-02 17:50:59 -0300

    Fixing broken jshint test

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
-import PapaParse from 'npm:papaparse';
-// import fs from 'ember-filesystem';
-
-
-
 
 export default Ember.Route.extend({
   setupController: function(controller, model) {
