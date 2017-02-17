a982f50143df32c3a3171abb737dbcd8748ea30b
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-02-17 12:19:12 -0300

    Decoupling things

--- app/routes/study.js
+++ app/routes/study.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import CsvParserMixin from 'ddah-ember/mixins/csv-parser';
+import UtilitiesMixin from 'ddah-ember/mixins/utilities';
 // import _ from 'lodash';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(CsvParserMixin, UtilitiesMixin, {
   model(params){
     let study = this.get('store').peekAll('study').findBy('slug', params.study_name);
     if (typeof study === "undefined"){
