bb80bd0fe13f18d7acea20be1a5b8890aa259bbe
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-03-17 18:28:10 -0300

    basic Area per Study calculations

---
+++ app/mixins/calculations.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import _ from 'lodash';
+
+export default Ember.Mixin.create({
+  getAverageFrom: function(promises, what_from_bills){
+    let sum = 0;
+    let bills = 0;
+
+    _.forEach(promises, function(p){
+      _.forEach(p.get('bills').toArray(), function(b){
+        sum = sum + parseInt(b.get(what_from_bills));
+        bills++;
+      });
+    });
+    if(bills){
+      return sum/bills;
+    }
+    return 0;
+  }
+});
