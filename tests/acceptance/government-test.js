2fa9fe59092c8b75b62fb289faa87b5f217c71e3
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-04-11 17:21:48 -0300

    [WIP] trying to fix acceptance test

--- tests/acceptance/government-test.js
+++ tests/acceptance/government-test.js
@@ -4,10 +4,10 @@ import moduleForAcceptance from 'ddah-ember/tests/helpers/module-for-acceptance'
 moduleForAcceptance('Acceptance | government');
 
 test('visiting /government', function(assert) {
-  visit('/government');
+  visit('/government/mishelle-bashelet');
 
   andThen(function() {
-    assert.equal(currentURL(), '/government');
+    assert.equal(currentURL(), '/government/mishelle-bashelet');
     assert.equal(find('.govs').length, 1);
   });
 });
