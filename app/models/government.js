bdec69a8a2311d54bd86e28b0a28d1de18cce964
damian-garrido <damian.garrido.munoz@gmail.com>
2017-04-11 12:31:33 -0300

    pass it has attributes like name and slug

--- app/models/government.js
+++ app/models/government.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  studies: DS.hasMany('study')
+  studies: DS.hasMany('study'),
+  slug: DS.attr('string', {defaultValue: function(e){
+    let gov = e.get('name');
+    return gov.replace(/\s+/g, '-').toLowerCase();
+    }})
 });
