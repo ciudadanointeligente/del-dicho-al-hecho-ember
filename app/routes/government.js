bfabc014d6f482d6fef5f81d0492ea8f665bbb3c
damian-garrido <damian.garrido.munoz@gmail.com>
2017-04-11 17:10:41 -0300

    WIP

--- app/routes/government.js
+++ app/routes/government.js
@@ -3,7 +3,14 @@ import CsvParserMixin from 'ddah-ember/mixins/csv-parser';
 import UtilitiesMixin from 'ddah-ember/mixins/utilities';
 
 export default Ember.Route.extend(CsvParserMixin, UtilitiesMixin, {
-  model(){
-    return this.get('store').peekAll('government');
+  model(params){
+    let gov = this.store.peekAll('government').findBy('slug', params.slug);
+    let filenames = [];
+
+    gov.get('studies').forEach(function(s){
+      filenames.push( s.get('filename') );
+    });
+
+    return this._arrayparseCsv(filenames,this.store);
   }
 });
