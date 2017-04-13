0a85173a3aa038f76c16088d5114b4284e8a79fa
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-04-13 17:07:39 -0300

    Fixing jshint errors

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -1,4 +1,5 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('route:index', 'Unit | Route | index', {
   needs: ['route:application', 'model:study', "model:promise", "model:government", "model:phase", "model:bill", "model:area", "model:priority", 'model:justification'],
@@ -18,7 +19,7 @@ test('/ route model() returns a study', function(assert) {
    "government": gov,
    "in_landing": true,
    "id": 1});
-   let s2 = route.get("store").createRecord("study", {"version": "Marzo",
+   route.get("store").createRecord("study", {"version": "Marzo",
     "filename": "test/Bachelet-2014-2018_Marzo-2016.csv",
     "year": 2016,
     "government": gov,
