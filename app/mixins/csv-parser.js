05206d1bae787754f66cd806ee1064e73c59a1b4
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-05-03 17:45:16 -0300

    Removing unnecesary console.log

--- app/mixins/csv-parser.js
+++ app/mixins/csv-parser.js
@@ -220,7 +220,6 @@ export default Ember.Mixin.create({
 
         let study = store.peekAll('study').findBy('filename', fn_without_root);
         studies.push(study);
-        console.log(config.rootURL + 'studies/' + fn_without_root);
         PapaParse.parse(config.rootURL + 'studies/' + fn_without_root, {
           download: true,
           header:true,
