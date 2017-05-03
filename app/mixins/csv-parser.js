09ce9587867272ac76cf454f67cd92c7a6ce65bd
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-05-03 17:24:55 -0300

    Console log

--- app/mixins/csv-parser.js
+++ app/mixins/csv-parser.js
@@ -220,6 +220,7 @@ export default Ember.Mixin.create({
 
         let study = store.peekAll('study').findBy('filename', fn_without_root);
         studies.push(study);
+        console.log(config.rootURL + 'studies/' + fn);
         PapaParse.parse(config.rootURL + 'studies/' + fn, {
           download: true,
           header:true,
