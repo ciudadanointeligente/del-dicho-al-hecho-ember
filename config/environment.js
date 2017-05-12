a75c7b50132f8240b888c4514b66b837fa498eb4
Lissett García Albornoz <lissett.garcia.a@gmail.com>
2017-05-12 16:56:21 -0300

    Content

--- config/environment.js
+++ config/environment.js
@@ -9,21 +9,21 @@ var governments =[{
 		"type": "Programa",
 		"img": "/img/bashelet-en-el-barquito.svg",
 		"year": 2015,
-		"version": "marzo",
+		"version": "Marzo",
 		"name": "Bachelet-2014-2018_Programa-2015.csv",
     "filename": "Bachelet-2014-2018_Programa-2015.csv"
 	}, {
 		"type": "Discurso",
 		"img": "/img/bashelet-en-el-avion.svg",
 		"year": 2015,
-		"version": "mayo",
+		"version": "Mayo",
 		"name": "Bachelet-2014-2018_Discurso-2015",
     "filename": "Bachelet-2014-2018_Discurso-2015.csv"
 	}, {
 		"type": "Programa",
 		"img": "/img/bashelet-con-la-fiera.svg",
 		"year": 2016,
-		"version": "marzo",
+		"version": "Marzo",
 		"name": "Bachelet-2014-2018_Programa-2016",
 		"filename": "Bachelet-2014-2018_Programa-2016.csv"
 	}, {
@@ -53,7 +53,7 @@ var governments =[{
 		"type": "Programa",
 		"img": "/img/pinera-en-el-auto.svg",
 		"year": 2012,
-		"version": "marzo",
+		"version": "Marzo",
 		"name": "Piñera-2010-2014_Programa-2012",
 		"filename": "Piñera-2010-2014_Programa-2012.csv"
 	}, {
@@ -67,21 +67,21 @@ var governments =[{
 		"type": "Programa",
 		"img": "/img/pinera-en-el-bicicleta.svg",
 		"year": 2013,
-		"version": "marzo",
+		"version": "Marzo",
 		"name": "Piñera-2010-2014_Programa-2013",
 		"filename": "Piñera-2010-2014_Programa-2013.csv"
 	}, {
 		"type": "Programa",
 		"img": "/img/pinera-en-el-bicicleta.svg",
 		"year": 2014,
-		"version": "marzo",
+		"version": "Marzo",
 		"name": "Piñera-2010-2014_Programa-2014",
 		"filename": "Piñera-2010-2014_Programa-2014.csv"
 	}, {
 		"type": "Discurso",
 		"img": "/img/pinera-en-el-bicicleta.svg",
 		"year": 2014,
-		"version": "mayo",
+		"version": "Mayo",
 		"name": "Piñera-2010-2014_Discurso-2014",
 		"filename": "Piñera-2010-2014_Discurso-2014.csv"
 	}]
