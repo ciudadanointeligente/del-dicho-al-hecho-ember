3f9288026779a20e35298b8e672db832a5fbcc03
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-05-23 17:41:59 -0400

    Fixing a test

--- app/components/study-alone.js
+++ app/components/study-alone.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
+import _ from 'lodash';
 
 export default Ember.Component.extend({
   didInsertElement: function() {
-    Ember.$('[data-toggle="tooltip"]').tooltip();
+     
+    let f = Ember.$('[data-toggle="tooltip"]').tooltip;
+    if(!_.isUndefined(f)){
+      f();
+    }
   }
 });
