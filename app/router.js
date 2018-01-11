de7caeb469098b617f5239899442142bdab4d8ee
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2018-01-11 12:25:05 -0200

    Agregandole google analytics a la cosa

--- app/router.js
+++ app/router.js
@@ -16,4 +16,14 @@ Router.map(function() {
   this.route('404');
 });
 
+Router.reopen({
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+  }.on('didTransition')
+});
+
+
 export default Router;
