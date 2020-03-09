7fe9de520f9fb4e7280f9add00a9af7b4450bd69
daquiroz <daquiroz@uc.cl>
2020-03-09 12:27:11 -0300

    fix cutoutPercentage in doughnut chart

--- app/helpers/heroByStudyType.js
+++ app/helpers/heroByStudyType.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function heroByStudyType(params/*, hash*/) {
   let study = params[0];
-  if(study.get('type') == "MovimientoSocial"){
+  if(study.get('type') === "MovimientoSocial"){
     return 'hero-movement';
   } else {
     return 'hero-studies';
