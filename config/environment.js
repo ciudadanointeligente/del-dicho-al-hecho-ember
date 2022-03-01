b5b24f19ee6ed4e3d05e58c654051a0a65874ce8
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2022-03-01 20:04:07 -0300

    act

---
+++ config/environment.js
@@ -0,0 +1,464 @@
+var governments =[
+		 {
+			 "name": "Sebastián Piñera II",
+			 "years": {'start': 2018, 'end': 2022},
+			 "color1": "#73F1FF",
+			 "color2": "#99F5FF",
+			 "color3": "#BFF8FF",
+			 "color4": "#E6FDFF",
+			 "studies": [
+	//  {
+  //   "type": "MovimientoSocial",
+  //   "color": "#F1ACFD",
+  //   "year": 2019,
+  //   "version": "Movilización Social",
+  //   "name": "Sebastián Piñera: Movilizaciones Sociales 2019",
+  //   "filename": "Pinera-2018-2022_Estallido-2019.csv",
+	// 	"title": "Los anuncios del Gobierno durante las movilizaciones sociales",
+  //   "description": "Revisa cuánto cumplió el gobierno de los anuncios legislativos realizados a raíz de las movilizaciones sociales.",
+  //   "fixed_result": 64,
+  //   "visible": true,
+	// 	"in_landing_2": false,
+  // },
+	{
+ "type": "Programa",
+ "color": "#73F1FF",
+ "year": 2021,
+ "version": "Programa de Gobierno",
+ "name": "Sebastián Piñera: Programa de Gobierno 2021",
+ "title": "Las promesas del Programa de Gobierno de Sebastián Piñera",
+ "filename": "Pinera-2018-2022_Programa-2021.csv",
+ "description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno.",
+ "fixed_result": 42,
+ "visible": true,
+ "in_landing": true,
+},
+{
+"type": "Programa",
+"color": "#73F1FF",
+"year": 2020,
+"version": "Programa de Gobierno",
+"name": "Sebastián Piñera: Programa de Gobierno 2020",
+"title": "Las promesas del Programa de Gobierno de Sebastián Piñera",
+"filename": "Pinera-2018-2022_Programa-2020.csv",
+"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno.",
+"fixed_result": 37,
+"visible": true,
+"in_landing": false,
+},
+{
+ "type": "Programa",
+ "color": "#73F1FF",
+ "year": 2019,
+ "version": "Programa de Gobierno",
+ "name": "Sebastián Piñera: Programa de Gobierno 2019",
+ "title": "Las promesas del Programa de Gobierno de Sebastián Piñera",
+ "filename": "Pinera-2018-2022_Programa-2019.csv",
+ "description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno.",
+ "fixed_result": 34,
+ "visible": true,
+ "in_landing_2": false,
+},
+{
+"type": "Discurso",
+"img": "/img/pinera-en-el-auto.svg",
+"color": "#99F5FF",
+"year": 2018,
+"version": "Cuenta Pública",
+"name": "Sebastián Piñera: Cuenta Pública 2018",
+"filename": "Pinera-2018-2022_Discurso-2019.csv",
+"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del gobierno de la Cuenta Pública 2018.",
+"title": "¿Cuánto cumplió el gobierno de Sebastián Piñera de la primera cuenta pública?",
+"fixed_result": 53,
+"visible": true,
+"in_landing_2": false,
+},
+{
+	"type": "Programa",
+	"color": "#99F5FF",
+	"year": 2018,
+	"version": "Programa de Gobierno",
+	"name": "Sebastián Piñera: Programa de Gobierno 2018",
+	"filename": "Pinera-2018-2022_Discurso-2019.csv",
+	"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno al primer año.",
+	"title": "¿Cuánto cumplió el gobierno de Sebastián Piñera al primer año?",
+	"fixed_result": 23,
+	"visible": true,
+	"in_landing": false,
+},
+	]},{
+	"name": "Michelle Bachelet II",
+	"years": {'start': 2014, 'end': 2018},
+	"color1": "#A973F4",
+	"color2": "#C099F7",
+	"color3": "#D7BFF9",
+	"color4": "#F0E6FD",
+	"studies": [{
+    "type": "Programa",
+    "img": "/img/bashelet-en-el-barquito.svg",
+    "color": "#A973F4",
+    "year": 2017,
+    "version": "Programa de Gobierno",
+    "name": "Michelle Bachelet: Programa de Gobierno 2017",
+    "filename": "Bachelet-2014-2018_Programa-2018.csv",
+    "description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno.",
+		"title": "¿Cuánto cumplió el gobierno de Michelle Bachelet?",
+    "fixed_result": 56,
+    "visible": true,
+  },{
+		"type": "Programa",
+		"img": "/img/bashelet-con-la-fiera.svg",
+		"color": "#C099F7",
+		"year": 2016,
+		"version": "Programa de Gobierno",
+		"name": "Michelle Bachelet: Programa de Gobierno 2016",
+		"filename": "Bachelet-2014-2018_Programa-2017.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno durante sus tres años de gestión.",
+		"title": "¿Cuánto cumplió el gobierno de Michelle Bachelet?",
+    "fixed_result": 47,
+		"visible": true,
+	}, {
+		"type": "Programa",
+		"img": "/img/bashelet-con-la-fiera.svg",
+		"color": "#D7BFF9",
+		"year": 2015,
+		"version": "Programa de Gobierno",
+		"name": "Michelle Bachelet: Programa de Gobierno 2015",
+		"filename": "Bachelet-2014-2018_Programa-2016.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno durante sus dos años de gestión.",
+		"title": "¿Cuánto cumplió el gobierno de Michelle Bachelet?",
+    "fixed_result": 39,
+    "visible": true,
+	}, {
+		"type": "Programa",
+		"img": "/img/bashelet-en-el-barquito.svg",
+		"color": "#F0E6FD",
+		"year": 2014,
+		"version": "Programa de Gobierno",
+		"name": "Michelle Bachelet: Programa de Gobierno 2014",
+    "filename": "Bachelet-2014-2018_Programa-2015.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno durante su primer año de gestión.",
+		"title": "¿Cuánto cumplió el gobierno de Michelle Bachelet?",
+    "fixed_result": 25,
+    "visible": true,
+	},{
+		"type": "Discurso",
+		"img": "/img/bashelet-con-la-fiera.svg",
+		"color": "#A973F4",
+		"year": 2017,
+		"version": "Cuenta Pública",
+		"name": "Michelle Bachelet: Cuenta Pública 2017",
+		"filename": "Bachelet-2014-2018_Discurso-2018.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno de la Cuenta Pública 2016.",
+		"title": "¿Cuánto cumplió el gobierno de Michelle Bachelet?",
+    "fixed_result": 50,
+		"visible": true,
+		},{
+		"type": "Discurso",
+		"img": "/img/bashelet-con-la-fiera.svg",
+		"color": "#C099F7",
+		"year": 2016,
+		"version": "Cuenta Pública",
+		"name": "Michelle Bachelet: Cuenta Pública 2016",
+		"filename": "Bachelet-2014-2018_Discurso-2017.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno de la Cuenta Pública 2016.",
+		"title": "¿Cuánto cumplió el gobierno de Michelle Bachelet?",
+    "fixed_result": 58,
+		"visible": true,
+		}, {
+		"type": "Discurso",
+		"img": "/img/bashelet-con-la-fiera.svg",
+		"color": "#D7BFF9",
+		"year": 2015,
+		"version": "Cuenta Pública",
+		"name": "Michelle Bachelet: Cuenta Pública 2015",
+		"filename": "Bachelet-2014-2018_Discurso-2016.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno de la Cuenta Pública 2015.",
+		"title": "¿Cuánto cumplió el gobierno de Michelle Bachelet?",
+    "fixed_result": 53,
+    "visible": true,
+	}, {
+		"type": "Discurso",
+		"img": "/img/bashelet-en-el-avion.svg",
+		"color": "#F0E6FD",
+		"year": 2014,
+		"version": "Cuenta Pública",
+		"name": "Michelle Bachelet: Cuenta Pública 2014",
+    "filename": "Bachelet-2014-2018_Discurso-2015.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno de la Cuenta Pública 2014.",
+		"title": "¿Cuánto cumplió el gobierno de Michelle Bachelet?",
+    "fixed_result": 44,
+    "visible": true,
+	}, ]
+}, {
+	"name": "Sebastián Piñera I",
+	"years": {'start': 2010, 'end': 2014},
+	"extra_info": "La primera medición de Del Dicho al Hecho se efectuó en 2012, por lo que los resultados al primer año no están disponibles.",
+	"color1": "#8D9EFF",
+	"color2": "#ACB9FF",
+	"color3": "#CBD3FE",
+	"color4": "#F0EFFF",
+	"studies": [{
+		"type": "Programa",
+		"img": "/img/pinera-en-el-bicicleta.svg",
+		"color": "#8D9EFF",
+		"year": 2013,
+		"version": "Programa de Gobierno",
+		"name": "Sebastián Piñera: Programa de Gobierno 2013",
+		"filename": "Pinera-2010-2014_Programa-2014.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno.",
+		"title": "¿Cuánto cumplió el gobierno de Sebastián Piñera?",
+    "fixed_result": 54,
+    "visible": true,
+	}, {
+		"type": "Programa",
+		"img": "/img/pinera-en-el-bicicleta.svg",
+		"color": "#ACB9FF",
+		"year": 2012,
+		"version": "Programa de Gobierno",
+		"name": "Sebastián Piñera: Programa de Gobierno 2012",
+		"filename": "Pinera-2010-2014_Programa-2013.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno durante sus tres años de gestión.",
+		"title": "¿Cuánto cumplió el gobierno de Sebastián Piñera?",
+    "fixed_result": 37,
+    "visible": true,
+	}, {
+		"type": "Programa",
+		"img": "/img/pinera-en-el-auto.svg",
+		"color": "#CBD3FE",
+		"year": 2011,
+		"version": "Programa de Gobierno",
+		"name": "Sebastián Piñera: Programa de Gobierno 2011",
+		"filename": "Pinera-2010-2014_Programa-2012.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno durante sus primer año de gestión.",
+		"title": "¿Cuánto cumplió el gobierno de Sebastián Piñera?",
+    "fixed_result": 26,
+    "visible": true,
+	}, {
+		"type": "Discurso",
+		"img": "/img/pinera-en-el-bicicleta.svg",
+		"color": "#8D9EFF",
+		"year": 2013,
+		"version": "Cuenta Pública",
+		"name": "Sebastián Piñera: Cuenta Pública 2013",
+		"filename": "Pinera-2010-2014_Discurso-2014.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno de la Cuenta Pública 2013.",
+		"title": "¿Cuánto cumplió el gobierno de Sebastián Piñera?",
+    "fixed_result": 54,
+    "visible": true,
+	}, {
+		"type": "Discurso",
+		"img": "/img/pinera-en-el-bicicleta.svg",
+		"color": "#ACB9FF",
+		"year": 2012,
+		"version": "Cuenta Pública",
+		"name": "Sebastián Piñera: Cuenta Pública 2012",
+		"filename": "Pinera-2010-2014_Discurso-2013.csv",
+    "fixed_result": 46,
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno de la Cuenta Pública 2012.",
+		"title": "¿Cuánto cumplió el gobierno de Sebastián Piñera?",
+    "visible": true,
+	}, {
+		"type": "Discurso",
+		"img": "/img/pinera-en-el-auto.svg",
+		"color": "#CBD3FE",
+		"year": 2011,
+    "fixed_result": 53,
+		"version": "Cuenta Pública",
+		"name": "Sebastián Piñera: Cuenta Pública 2011",
+		"filename": "Pinera-2010-2014_Discurso-2012.csv",
+		"description": "Revisa aquí el porcentaje de cumplimiento de las promesas del programa de gobierno de la Cuenta Pública 2011.",
+		"title": "¿Cuánto cumplió el gobierno de Sebastián Piñera?",
+    "visible": true,
+	},]
+}];
+module.exports = function(environment) {
+  var orders = {
+    1: 'primer',
+    2: 'segundo',
+    3: 'tercer',
+    4: 'cuarto'
+  };
+  var area_images = {
+    'Agricultura': 'img/i-agr.svg',
+		'Ciencia y Tecnología': 'img/i-cie.svg',
+    'Cultura': 'img/i-cul.svg',
+    'Defensa': 'img/i-def.svg',
+    'Democracia': 'img/i-dem.svg',
+		'Deporte': 'img/i-dep.svg',
+    'Desarrollo Social': 'img/i-des.svg',
+    'Economía y Comercio': 'img/i-eco.svg',
+    'Educación': 'img/i-edu.svg',
+    'Energia': 'img/i-ene.svg',
+    'Equidad de Género': 'img/i-equ.svg',
+    'Estado': 'img/i-est.svg',
+    'Infancia y Juventud': 'img/i-inf.svg',
+    'Justicia y Derechos Humanos': 'img/i-jus.svg',
+    'Medio Ambiente': 'img/i-med.svg',
+    'Mineria': 'img/i-min.svg',
+    'Obras Públicas': 'img/i-obr.svg',
+    'Pueblos Indígenas': 'img/i-pue.svg',
+    'Salud': 'img/i-sal.svg',
+    'Seguridad Ciudadana': 'img/i-seg.svg',
+    'Trabajo y Previsión Social': 'img/i-tra.svg',
+    'Transporte y Telecomunicaciones': 'img/i-tran.svg',
+    'Vivienda y Urbanismo': 'img/i-viv.svg',
+    'default': 'img/i-def.svg',
+  };
+  var matcher = {
+    'area': {
+      'name': 'area',
+      'id': {
+        'fieldToGetIdFrom': 'area'
+      }
+    },
+    'promise': {
+      'content': 'promesa',
+      'number': 'uid',
+      'title': 'titulo',
+      'ja-why': 'ja_porque',
+      'jc-why': 'jn_porque',
+      'coherence-level': 'escala_de_coherencia',
+      'id': {
+        'fieldToGetIdFrom': 'uid'
+      },
+      'relationships':[
+        "area"
+      ]
+    },
+    'bill': {
+      'name': 'boletin',
+      'title': 'titulo_proyecto',
+      'url': 'link',
+      'justification': 'justificacion_avance',
+      'year': 'Ano',
+      'version': 'Version',
+      'id': {
+        'fieldToGetIdFrom': 'boletin'
+      },
+      'relationships':[
+        "phase",
+        "priority",
+      ],
+      'chekIsEmpty': 'boletin',
+    },
+    'justification': {
+      'justification': 'justificacion_avance',
+      'id': {
+        'fieldToGetIdFrom': 'justificacion_avance'
+      },
+      relationships: [
+        'promise',
+        'bill'
+      ]
+    }
+  };
+  var priorities = {
+    'priorities': [
+      {
+        'countColumnName': "simple",
+        'name': 'Simple'
+      },
+      {
+        'countColumnName': "suma",
+        'name': 'Suma'
+      },
+      {
+        'countColumnName': "inmediata",
+        'name': 'Inmediata'
+      },
+    ]
+  };
+  var phases = {
+    'columnName': 'nombre_avance',
+    "phases":[
+    {
+      'name': 'Primer Tramite',
+      'fullfilment': 40,
+    }, {
+      'name': 'Segundo Tramite',
+      'fullfilment': 70,
+    }, {
+      'name': 'Veto',
+      'fullfilment': 40,
+    }, {
+      'name': 'Insistencia',
+      'fullfilment': 40,
+    }, {
+      'name': 'Rechazado Retirado',
+      'fullfilment': 0,
+    }, {
+      'name': 'Comisión Mixta',
+      'fullfilment': 70,
+    }, {
+      'name': 'Tribunal Constitucional',
+      'fullfilment': 100,
+    }, {
+      'name': 'Aprobación Presidencial',
+      'fullfilment': 90,
+    }, {
+      'name': 'Promulgado',
+      'fullfilment': 100,
+    }, {
+      'name': 'Tercer Tramite',
+      'fullfilment': 70,
+    }
+  ]};
+	var comparisonsAreVisible = true;
+	var launched = false;
+
+  var ENV = {
+    modulePrefix: 'ddah-ember',
+    rrssUrl: 'https://docs.google.com/document/d/1amWdc8mGXMPDYhJVGsDINjXi1ibFElQlmmVxR6ria4M/edit?usp=sharing',
+    matcher: matcher,
+    orders:orders,
+    governments: governments,
+    government_length: 4,
+		useOnlyJsons: true,
+    areaImages: area_images,
+    comparable_with_other_gov: 'Programa',
+    priorities: priorities,
+    phases: phases,
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+		comparisonsAreVisible: true,
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': truea
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true; 2014-2018
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/';
+  }
+
+  return ENV;
+};
