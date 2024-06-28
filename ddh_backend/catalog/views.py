229e74cf52085964ddf443c4e20bf1c136a54f4f
Rafael Zazueta <jzazueta@alioit.com>
2024-06-28 12:42:34 -0700

    add bulk page

--- ddh_backend/catalog/views.py
+++ ddh_backend/catalog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def bulk(request):
+    template = loader.get_template('bulk.html')
+    return HttpResponse(template.render())
--- ddh_backend/ddh_backend/urls.py
+++ ddh_backend/ddh_backend/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
+from catalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bulk/', views.bulk, name='bulk'),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
