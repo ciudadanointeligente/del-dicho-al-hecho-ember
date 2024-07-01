8ce14dc0526fcf0552e1e7e79623c45a16b58f64
Rafael Zazueta <jzazueta@alioit.com>
2024-07-01 16:49:52 -0700

    update utils file

--- ddh_backend/catalog/utils.py
+++ ddh_backend/catalog/utils.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Count
+from models import Government
 import json
 import re
 
@@ -35,7 +36,7 @@ mapKeys = {
     }
 }
 
-def createRecords(request, Model, typeCatalog):
+def createRecords(request, model, typeCatalog):
     response = 'Success'
     error = None
     validKeys = mapKeys.get(typeCatalog)
@@ -50,7 +51,7 @@ def createRecords(request, Model, typeCatalog):
         messages.error(request, error)
         return render(request, "bulk.html", context)
     if type(elementList) == list:
-        if findDuplicate(Model, elementList):
+        if findDuplicate(model, elementList):
             error = 'Duplicated name in'
             response = None
         else:
@@ -62,10 +63,40 @@ def createRecords(request, Model, typeCatalog):
     else:
         error = 'Invalid phase JSON'
         response = None
-    ##if not error:
-    ##    for elementItem in elementList:
-    ##       newRecord = Model(name=elementItem.get('name'), fullfilment=elementItem.get('fullfilment'))
-    ##        newRecord.save()   
+    if not error:
+        for elementItem in elementList:
+            if typeCatalog == 'phase':
+                newRecord = model(
+                    name=elementItem.get('name'), 
+                    fullfilment=elementItem.get('fullfilment')
+                )
+            elif typeCatalog == 'government':
+                newRecord = model(
+                    name=elementItem.get('name')
+                    start_year=elementItem.get('start_year'),
+                    end_year=elementItem.get('end_year'),
+                    color1=elementItem.get('color1'),
+                    color2=elementItem.get('color2'),
+                    color3=elementItem.get('color3'),
+                    color4=elementItem.get('color4')
+                )
+            elif typeCatalog == 'study':
+                newRecord = model(
+                    type=elementItem.get('type'),
+                    img=elementItem.get('img'),
+                    color=elementItem.get('color'),
+                    year=elementItem.get('year'),
+                    version=elementItem.get('version'),
+                    name=elementItem.get('name'),
+                    filename=elementItem.get('filename'),
+                    description=elementItem.get('description'),
+                    title=elementItem.get('title'),
+                    fixed_result=elementItem.get('fixed_result'),
+                    visible=elementItem.get('visible'),
+                    in_landing=elementItem.get('in_landing'),
+                    government=getGovernment(elementItem.get('government'))
+                )
+            newRecord.save()   
     if error:
         context = {'selected': typeCatalog}
         messages.error(request, error)
@@ -77,3 +108,6 @@ def createRecords(request, Model, typeCatalog):
 def findDuplicate(model, elementList):
     isDuplicated = model.objects.filter(name__in=[element['name'] for element in elementList]).values('name').annotate(count=Count('name')).filter(count__gt=0).exists()
     return isDuplicated
+
+def getGovernment(id):
+    return Government.objects.get(pk=id)
